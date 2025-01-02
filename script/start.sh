2fe0b991dc4e5b91d36600b0835f26e76cb28023
skyArony <1450872874@qq.com>
2025-01-03 02:19:33 +0800

    feat: 修改 PostgreSQL 端口映射并简化启动脚本

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,1 @@
-docker compose -p nest-quick-demo --env-file ../.env pull
-docker compose -p nest-quick-demo --env-file ../.env down --remove-orphans
 docker compose -p nest-quick-demo --env-file ../.env up -d
