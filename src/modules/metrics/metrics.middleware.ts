7d9fe284e02fe52a92eeba84e819c76b337b0d3c
skyArony <1450872874@qq.com>
2025-01-03 21:00:29 +0800

    feat: 重命名 MetricsMiddleware 中的指标属性，以提高代码可读性

--- src/modules/metrics/metrics.middleware.ts
+++ src/modules/metrics/metrics.middleware.ts
@@ -7,10 +7,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class MetricsMiddleware implements NestMiddleware {
   @InjectMetric(Metrics.HTTP_REQUESTS_TOTAL)
-  private readonly http_requests_total: Counter;
+  private readonly httpRequestsTotal: Counter;
 
   @InjectMetric(Metrics.HTTP_REQUEST_DURATION_SECONDS)
-  private readonly http_request_duration_seconds: Histogram;
+  private readonly httpRequestDurationSeconds: Histogram;
 
   use(req: Request, res: Response, next: NextFunction): void {
     const startTime = Date.now();
@@ -18,13 +18,13 @@ export class MetricsMiddleware implements NestMiddleware {
 
     // 监听响应完成事件，记录请求成功情况
     res.on('finish', () => {
-      this.http_requests_total.inc({
+      this.httpRequestsTotal.inc({
         method,
         path,
         status: res.statusCode,
       });
 
-      this.http_request_duration_seconds.observe(
+      this.httpRequestDurationSeconds.observe(
         { method, path },
         Date.now() - startTime,
       );
@@ -32,13 +32,13 @@ export class MetricsMiddleware implements NestMiddleware {
 
     // 监听请求错误
     res.on('error', (error: any) => {
-      this.http_requests_total.inc({
+      this.httpRequestsTotal.inc({
         method,
         path,
         status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
       });
 
-      this.http_request_duration_seconds.observe(
+      this.httpRequestDurationSeconds.observe(
         { method, path },
         Date.now() - startTime,
       );
